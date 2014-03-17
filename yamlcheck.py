2d3cb9ce77ac1afefd57510a51fc76706d702b8d
Kristian Botnen <Kristian.Botnen@uib.no>
2014-03-17 14:25:47 +0100

    some usage instructions added

--- yamlcheck.py
+++ yamlcheck.py
@@ -1,6 +1,18 @@
 # -*- coding: utf-8 -*-
-#!/usr/bin/python
-#__author__ = 'Kristian Botnen'
+# A simple file that reads and check if your YAML files are correct
+#
+# Author: Kristian Botnen
+# Email: kristian.botnen@adm.uib.no
+# License: The MIT License
+# Requirements: pip install pyyaml
+#
+# Usage:
+#
+# $ python yamlcheck.py -f test.yaml
+# or
+# $ python yamlcheck.py -f /path/to/folder
+#
+# 
 
 import os
 import argparse
@@ -9,9 +21,9 @@ import yaml
 parser = argparse.ArgumentParser(description='Process some yaml.')
 parser.add_argument('-f', '--f', metavar='', help='file or folder', action="store")
 args = parser.parse_args()
-
 inputstring = args.f
 
+#This method tries to load a given filename, and parse it with pyyaml.
 def parsefile(filename):
   try: 
     yamlfile = open(filename, 'r')
@@ -23,12 +35,12 @@ def parsefile(filename):
   except (IOError, TypeError) as e:
     print "I/O error:" + str(e)
     
-#Check if argument is folder or single file
+#Check if argument is single file
 if os.path.isfile(inputstring):
   filename = inputstring
   print "Parsing the file:" + filename
   parsefile(filename)
-
+#Check if the argument is folder
 elif os.path.isdir(inputstring):
   foldername = inputstring
   filenamearray = []
@@ -39,6 +51,6 @@ elif os.path.isdir(inputstring):
       if file.lower().endswith("yaml"):
         filenamearray.append(os.path.join(root, file))
         parsefile((os.path.join(root, file)))
-    
+#Can not determine what the user gave as argument, probably a nonexisting file / folder    
 else:
   print "Cant determine if input is file or folder:" + inputstring
